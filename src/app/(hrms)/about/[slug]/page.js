d599b68bd782c9939968486697a7f91a82660f32
ajha-digi <ajha@digisprint.com>
2023-03-30 23:02:10 +0530

    Grouping concept in routing

---
+++ src/app/(hrms)/about/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <main>
+       inside hrms slug !!
+      </main>
+    )
+  }
---
+++ src/app/(portal)/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Second() {
+    return (
+      <main>
+        hello Dashboard  world for portal !!
+      </main>
+    )
+  }
